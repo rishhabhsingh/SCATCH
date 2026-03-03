2c9f6908ca4fc865fbb3f84fc80ac92778ba0127
rishhabhsingh <singhrishabh2308@gmail.com>
2026-03-03 20:24:55 +0530

    PHASE 0 completed

---
+++ server/config/redis.js
@@ -0,0 +1,8 @@
+const Redis = require('ioredis')
+
+const redis = new Redis(process.env.REDIS_URL)
+
+redis.on('connect', () => console.log('Redis Connected'))
+redis.on('error', (err) => console.error(`Redis Error: ${err}`))
+
+module.exports = redis
---
+++ server/utils/generateToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const generateAccessToken = (userId, role) => {
+  return jwt.sign(
+    { userId, role },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: process.env.JWT_ACCESS_EXPIRES }
+  )
+}
+
+const generateRefreshToken = (userId) => {
+  return jwt.sign(
+    { userId },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: process.env.JWT_REFRESH_EXPIRES }
+  )
+}
+
+module.exports = { generateAccessToken, generateRefreshToken }
