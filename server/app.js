dbca13238daada4bfd1ff376697c905754c369fb
rishhabhsingh <singhrishabh2308@gmail.com>
2026-04-01 16:38:15 +0530

    fix: update CORS for production

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(helmet())
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL,
+    'https://scatchbyrs.vercel.app',
     'http://localhost:5173',
   ],
   credentials: true,
